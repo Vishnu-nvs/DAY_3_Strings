cdaa4abbc3350c248af7dff02442c8590cb4823a
vishnu <vishnunvs0003@gmail.com>
2025-06-14 07:48:04 +0530

    Task some code changes

--- Front_Slice_Task4.py
+++ Front_Slice_Task4.py
@@ -1,10 +1,14 @@
 s="Ariyah Arias"
 #Ex-Output:1.Aar
-print(s[0:9:4])         
+print(s[0:9:4])   
+# or  print(i[0:2:1] i[10:1])      
            
 #Ex-Output:2.yaas
 #Note:output is error becuase stepvalues diffrent that'swhy we are using looping concept
-
+#y=3,a=4,a=7,s=11
+#for x in range (len(i));
+#if x=3 or x==4 or x ==7 or x==7;
+#print(i[x])
 
 #Ex-Output:3.Aras
 #Note:output is error becuase stepvalues diffrent that'swhy we are using looping concept
